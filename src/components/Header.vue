e20ffbd4dadbf4ff412f4de2fbc5f72ee425fdc4
Ali Dev <ali.rn.dev@gmail.com>
2020-10-03 18:49:02 -0400

    everything is working as it is supposed to

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <b-nav tabs>
+        <b-nav-item disabled><strong>Fancy Quiz App</strong></b-nav-item>
+        <b-nav-item disabled>Counter: {{numCorrect}}/{{numTotal}}</b-nav-item>
+    </b-nav>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
